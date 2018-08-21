1317cf79d50115454b85b61911652d34dce2028e
Carl W. Hunt <cwhunt@litlog.io>
2018-08-21 09:37:19 -0700

    added React requirements

--- src/templates/catalog-entry.js
+++ src/templates/catalog-entry.js
@@ -15,43 +15,43 @@ export default ({ data }) => {
     <Layout>
       <div
         id="document"
-        class="document "
-        itemscope
-        itemtype="http://schema.org/Thing"
+        className="document "
+        itemScope
+        itemType="http://schema.org/Thing"
       >
         <div id="{entry.frontmatter.id}">
-          <h1 itemprop="name">{entry.frontmatter.title}</h1>
-          <dl class="dl-horizontal  dl-invert">
-            <dt class="ac-title_display">Title:</dt>
-            <dd class="ac-title_display">{entry.frontmatter.title}</dd>
-            <dt class="ac-artist_facet">Artist:</dt>
-            <dd class="ac-artist_facet">
+          <h1 itemProp="name">{entry.frontmatter.title}</h1>
+          <dl className="dl-horizontal  dl-invert">
+            <dt className="ac-title_display">Title:</dt>
+            <dd className="ac-title_display">{entry.frontmatter.title}</dd>
+            <dt className="ac-artist_facet">Artist:</dt>
+            <dd className="ac-artist_facet">
               <Link to={`/artists/${_.kebabCase(entry.frontmatter.artist)}/`}>{entry.frontmatter.artist}</Link>
             </dd>
-            <dt class="ac-date_display">Date:</dt>
-            <dd class="ac-date_display">{entry.frontmatter.year}</dd>
-            <dt class="ac-description_display">Description:</dt>
-            <dd class="ac-description_display">
+            <dt className="ac-date_display">Date:</dt>
+            <dd className="ac-date_display">{entry.frontmatter.year}</dd>
+            <dt className="ac-description_display">Description:</dt>
+            <dd className="ac-description_display">
               {renderAst(entry.htmlAst)}
             </dd>
-            <dt class="ac-type_facet">Type:</dt>
-            <dd class="ac-type_facet">
+            <dt className="ac-type_facet">Type:</dt>
+            <dd className="ac-type_facet">
               <Link to={`/types/${_.kebabCase(entry.frontmatter.type)}/`}>{entry.frontmatter.type}</Link>
             </dd>
-            <dt class="ac-medium_facet">Medium:</dt>
-            <dd class="ac-medium_facet">
+            <dt className="ac-medium_facet">Medium:</dt>
+            <dd className="ac-medium_facet">
               <Link to={`/media/${_.kebabCase(entry.frontmatter.medium)}/`}>{entry.frontmatter.medium}</Link>
             </dd>
-            <dt class="ac-collection_facet">Collection:</dt>
-            <dd class="ac-collection_facet">
+            <dt className="ac-collection_facet">Collection:</dt>
+            <dd className="ac-collection_facet">
               <Link to={`/collections/${_.kebabCase(entry.frontmatter.collection)}/`}>{entry.frontmatter.collection}</Link>
             </dd>
-            <dt class="ac-accession_display">
+            <dt className="ac-accession_display">
               Object Identifier (Accession #):
             </dt>
-            <dd class="ac-accession_display">{entry.frontmatter.id}</dd>
-            <dt class="ac-img_original_display">Picture{entry.frontmatter.figure.length === 1 ? "" : "s"}:</dt>
-            <dd class="ac-img_original_display">
+            <dd className="ac-accession_display">{entry.frontmatter.id}</dd>
+            <dt className="ac-img_original_display">Picture{entry.frontmatter.figure.length === 1 ? "" : "s"}:</dt>
+            <dd className="ac-img_original_display">
               <Gallery images={entry.frontmatter.figure} />
             </dd>
           </dl>
