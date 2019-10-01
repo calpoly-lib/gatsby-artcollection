483a0c310f9ecabe9ada7ebf4214d6b624aa14da
Carl W. Hunt <cwhunt@users.noreply.github.com>
2019-10-01 15:31:52 -0700

    Revert "Modify assets/fonts/colors to match branding guidelines"

--- src/components/header.js
+++ src/components/header.js
@@ -26,39 +26,38 @@ export default class Header extends Component {
   }
   render() {
     return (
-      <div id="title-navbar">
+      <div>
         <div id="title">
           <a className="cp-logo" href="https://www.calpoly.edu/">
-            <img src="/assets/cp_artcollection.png" alt="Cal Poly logo" height="25" />
+            <img src="/assets/calpoly.jpg" alt="Cal Poly logo" height="25" />
           </a>
           <Link to="/" className="navbar-brand">University Art Collection</Link>
         </div>
-
-          <Navbar color="light" light expand="md">
-            <NavbarToggler onClick={this.toggle} className="mr-2" />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <Link to="/" className="nav-link">Home</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to="/about/" className="nav-link">About</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to="/policies/" className="nav-link">Policies</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to="/class_visits/" className="nav-link">Class Visits</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to="/rfp/" className="nav-link">Request for Proposals</Link>
-                </NavItem>
-                <NavItem>
-                  <Search className="nav-link" />
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Navbar>
+        <Navbar color="light" light expand="md">
+          <NavbarToggler onClick={this.toggle} className="mr-2" />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/" className="nav-link">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/about/" className="nav-link">About</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/policies/" className="nav-link">Policies</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/class_visits/" className="nav-link">Class Visits</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/rfp/" className="nav-link">Request for Proposals</Link>
+              </NavItem>
+              <NavItem>
+                <Search className="nav-link" />
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
