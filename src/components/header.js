acc03c878a2ac60907605f4386da28759d3cc706
Eric Newcomer <enewcome@calpoly.edu>
2019-10-03 09:51:00 -0700

    Use assets prefix

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,6 @@ import {
 
 // import Browse from './browse'
 import Search from './search';
-// import Cal Poly Logo
-import cp_logo from '../../static/assets/cp_artcollection.png';
 
 export default class Header extends Component {
   constructor(props) {
@@ -31,7 +29,7 @@ export default class Header extends Component {
       <div id="title-navbar">
         <div id="title">
           <a className="cp-logo" href="https://www.calpoly.edu/">
-            <img src={ cp_logo } alt="Cal Poly logo" height="25" />
+            <img src="/assets/cp_artcollection.png" alt="Cal Poly logo" height="25" />
           </a>
           <Link to="/" className="navbar-brand">University Art Collection</Link>
         </div>
